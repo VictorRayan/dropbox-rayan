7d4d4b0cdeb005fcc31d7aac577ed69fb347fcec
VictorRayan <victorrayansouzaramos@gmail.com>
2022-06-26 13:31:42 -0300

    Dropbox API file handler library --version: 1.0

---
+++ src/getEnvDropboxTokens.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace VictorRayan\DropboxRayanVrsrb;
+
+class getEnvDropboxTokens{
+
+
+    public function app_key(){
+
+        $app_key = $_ENV['DROPBOX_APP_KEY'];
+        $app_key = "";
+
+        return $app_key();
+    }
+
+    public function app_secret(){
+
+        $app_secret = $_ENV['DROPBOX_APP_KEY'];
+        return $app_secret();
+    }
+
+    public function app_refresh_token(){
+
+        $app_refresh_token = $_ENV['DROPBOX_REFRESH_TOKEN'];
+        return $app_refresh_token;
+
+
+    }
+}
+
+
+
+?>
